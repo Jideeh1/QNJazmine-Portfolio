617efea3adf7ac97390937fba92041db88ec759d
John Daniel Absalud <keonagjd@gmail.com>
2025-05-05 20:17:33 +0800

    Debug Lines + Fallback Images

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 // Comments just in case someone snoop on my code through inspect element
-console.log("If you're here to snoop on my messy code, reconsider before your head starts hurting.\n\nSeriously tho, stop. If you just want to take some snippets or look at the whole code, it's available in my github.\n\nhttps://github.com/Jideeh1/QNJazmine-Portfolios \n\nJust don't complain when you see how messy the code is. ーｗー \n\nSincerely, Jideeh");
+console.log("If you're here to scrutinize the logic of my code, reconsider before your head starts hurting.\n\nIt'd be a big mistake trying to read my HTML. My css however... I'm proud of the way I arranged it.\n\nTake a look at my css thru the sources.\n\nSeriously tho, if you just want to take some snippet, fork some js functions, or insepct the code, it's available in my github.\n\nhttps://github.com/Jideeh1/QNJazmine-Portfolios \n\nJust don't complain when you see how messy the directory is. ーｗー \n\nYours truly, Jideeh");
 
 document.querySelector("form").addEventListener("submit", function(e) {
     e.preventDefault();
-  
     const form = e.target;
     const formData = new FormData(form);
   
@@ -17,13 +16,13 @@ document.querySelector("form").addEventListener("submit", function(e) {
     .then(response => {
       if (response.ok) {
         form.reset();
-        console.log("Email sent!");
+        console.log("Hell Yeah!"); //Yes, I still debug using console logs and alerts. Fuck off.
       } else {
-        console.error("Error sending email.");
+        console.error("WHY ARE YOU NOT WORKING?"); //Yes, I still debug using console logs and alerts. Fuck off.
       }
     })
     .catch(error => {
-      console.error("Submission error:", error);
+      console.error("Fuck this error:", error); //Yes, I still debug using console logs and alerts. Fuck off.
     });
   });
   
@@ -31,6 +30,8 @@ document.querySelector("form").addEventListener("submit", function(e) {
 function startFlyAnimation() {
     const mail = document.querySelector('.mail-container');
     const wings = document.querySelectorAll('.wing');
+
+    console.log("Wheee! I'm flying!"); //Yes, I still debug using console logs and alerts. Fuck off.
   
     mail.style.animation = 'none';
     void mail.offsetWidth;
@@ -45,10 +46,16 @@ function startFlyAnimation() {
   }
   
   function confirmEmail() {
+
+    console.log("Uhh, email?"); //Yes, I still debug using console logs and alerts. Fuck off.
+
+
     if (confirm("Do you want to proceed to email?")) {
         window.location.href = "mailto:qnjazminebusiness@gmail.com";
+
+        
     }
     else {
-        console.log('Edi wag.');
+        console.log('No? Okay.'); //Yes, I still debug using console logs and alerts. Fuck off.
       }
 }
